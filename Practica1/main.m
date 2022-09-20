e9a315bec90198a3f7d5deb78dfcf3ccba4ae468
David Egea Hernández <201805328@alu.comillas.edu>
2022-09-20 21:14:31 +0200

    editar figuras P1

--- Practica1/main.m
+++ Practica1/main.m
@@ -38,7 +38,7 @@ phi2 = [zeros(1,length(t)/2) ones(1,length(t)/2)];
 % Representación de las señales
 figure;
 subplot(3,1,1);
-plot(t,s1);
+plot(t,s1,"b");
 xlabel("t (ms)");   
 ylim([-1 1])
 title("señal de entrada s1(t)");
@@ -79,46 +79,93 @@ Ts = T/20;
 
 % Se genera el vector de tiempo
 t = linspace(0,T,T/Ts);
-% Primera señal
-s1 = ones(1,length(t));
-% Primera señal
-s2 = [ones(1,length(t)/2) -ones(1,length(t)/2)];
+
+% Simbolos s1 y s2
+s1.o = ones(1,length(t));
+s2.i = [ones(1,length(t)/2) -ones(1,length(t)/2)];
+
 phi1 = [ones(1,length(t)/2) zeros(1,length(t)/2)];
 phi2 = [zeros(1,length(t)/2) ones(1,length(t)/2)];
 
 % Generamos el ruido blanco de distintas potencias
-s1_noise_5dB = awgn(s1,5);
-s2_noise_5dB = awgn(s1,5);
-
+s1.snr_5dB = awgn(s1.o,5);
+s2.snr_5dB = awgn(s2.i,5);
+s1.snr_10dB = awgn(s1.o,10);
+s2.snr_10dB = awgn(s2.i,10);
+s1.snr_15dB = awgn(s1.o,15);
+s2.snr_15dB = awgn(s2.i,15);
 
 % Llamada a la función de correlación
-[y1_s1_10,y2_s1_10] = correlatorType(T,Ts,s1_noise_5dB);
-[y1_s2_10,y2_s2_10] = correlatorType(T,Ts,s2_noise_5dB);
+[y1.s1.snr_5dB,y2.s1.snr_5dB] = correlatorType(T,Ts,s1.snr_5dB);
+[y1.s2.snr_5dB,y2.s2.snr_5dB] = correlatorType(T,Ts,s2.snr_5dB);
+[y1.s1.snr_10dB,y2.s1.snr_10dB] = correlatorType(T,Ts,s1.snr_10dB);
+[y1.s2.snr_10dB,y2.s2.snr_10dB] = correlatorType(T,Ts,s2.snr_10dB);
+[y1.s1.snr_15dB,y2.s1.snr_15dB] = correlatorType(T,Ts,s1.snr_15dB);
+[y1.s2.snr_15dB,y2.s2.snr_15dB] = correlatorType(T,Ts,s2.snr_15dB);
+
 % Comparación entre la señal con y sin ruido
 figure;
-plot(t,s1,"b");
+% Simbolo s1
+subplot(2,1,1);
+plot(t,s1.o,"b");
 hold on;
-plot(t,s1_noise_5dB,"r");
+plot(t,s1.snr_5dB);
+plot(t,s1.snr_10dB);
+plot(t,s1.snr_15dB);
 xlabel("t (ms)");   
-title("Comparativa señal con y sin ruido");
-legend("s1(t)","s1_noise(t)(SNR=5dB)")
+title("Señal de s1 con y sin ruido");
+legend(["original","SNR=5dB", "SNR=10dB", "SNR=15dB"],'Location','southeast');
+% Simbolo s2
+subplot(2,1,2);
+plot(t,s2.i,"b");
+hold on;
+plot(t,s2.snr_5dB);
+plot(t,s2.snr_10dB);
+plot(t,s2.snr_15dB);
+xlabel("t (ms)");   
+title("Señal de s2 con y sin ruido");
+legend(["original","SNR=5dB", "SNR=10dB", "SNR=15dB"],'Location','southeast');
+sgtitle("Comparativa señales con y sin ruido");
 
-% Representación de los resultados con ruido
+% Representación de los resultados con ruido (para 5dB)
 figure;
-subplot(3,1,1);
-plot(t,s1_noise_5dB);
+% Simbolo s1
+subplot(3,2,1);
+plot(t,s1.o);
+hold on;
+plot(t,s1.snr_5dB);
 xlabel("t (ms)");   
-ylim([-2 2])
+ylim([-2 2]);
+legend(["original","SNR=5dB"],'Location','southeast');
 title("señal de entrada s1(t) con ruido (SNR=5dB)");
-subplot(3,1,2);
-plot(t,y1_s1_10*phi1,"r*-");
+subplot(3,2,3);
+plot(t,y1.s1.snr_5dB*phi1,"r*-");
 ylim([-2 2])
-title(" y1(t) salida del correlador 1");
-subplot(3,1,3);
-plot(t,y2_s1_10*phi2,"g-o");
+title("y1(t) salida del correlador 1");
+subplot(3,2,5);
+plot(t,y2.s1.snr_5dB*phi2,"g-o");
 xlabel("t (ms)");
 ylim([-2 2])
 title("y2(t) salida del correlador 2 ");
+% Simbolo s2
+subplot(3,2,2);
+plot(t,s2.i,"b");
+hold on;
+plot(t,s2.snr_5dB);
+xlabel("t (ms)");   
+ylim([-2 2]);
+legend(["original","SNR=5dB"],'Location','southeast');
+title("señal de entrada s2(t) con ruido (SNR=5dB)");
+subplot(3,2,4);
+plot(t,y1.s2.snr_5dB*phi1,"r*-");
+ylim([-2 2])
+title("y1(t) salida del correlador 1");
+subplot(3,2,6);
+plot(t,y2.s2.snr_5dB*phi2,"g-o");
+xlabel("t (ms)");
+ylim([-2 2])
+title("y2(t) salida del correlador 2 ");
+sgtitle("Salidas demodulador (y1, y2) para señales s1 y s2");
 
 %% 3. Salida del demodulador  - Ejercicio 3.1
 
