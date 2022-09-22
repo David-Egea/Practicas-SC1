65a568e15db94893547e661f5254f9565c3b4424
David Egea Hernández <201805328@alu.comillas.edu>
2022-09-22 08:13:02 +0200

    subir inicio P2

---
+++ Practica2/coefs.m
@@ -0,0 +1,14 @@
+function [c1,c2] = coefs(s,b1,b2)
+% Recibe:
+%   * s: simbolo de entrada
+%   * b1: primera señal base ortonormal 
+%   * b2: segunda señal base ortonormal
+% Devuelve los coeficientes c1 y c2 de las bases b1 y b2 asociadas al simbolo s
+%   * c1: Coeficiente asociado a b1
+%   * c2: Coeficiente asociado a b2
+
+c1 = sum(s.*b1);
+c2 = sum(s.*b2);
+
+end
+
