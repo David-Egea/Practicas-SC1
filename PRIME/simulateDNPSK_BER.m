3f873ed9dbc3d141789301a817aa76a5c5568f40
David-Egea <egeahernandezdavid@gmail.com>
2022-12-05 11:25:10 +0100

    actualizar cambios PRIME
    Co-authored-by: davidco27 <davidco27@users.noreply.github.com>

--- PRIME/simulateDNPSK_BER.m
+++ PRIME/simulateDNPSK_BER.m
@@ -1,4 +1,4 @@
-function simBER = simulateDNPSK_BER(SNR_dB,N,NFFT,Nofdm,N_pay,nBits)
+function simBER = simulateDNPSK_BER(SNR_dB,N,NFFT,Nofdm,N_pay,nBits,h)
     % Función del bloque scrambler. Recibe:
         % SNR_dB: Vector de valores de SNR
         % N: Niveles de modulación 
@@ -6,6 +6,7 @@ function simBER = simulateDNPSK_BER(SNR_dB,N,NFFT,Nofdm,N_pay,nBits)
         % Nofdm: Número de símbolos OFDM
         % N_pay: Numero de portadoras OFDM
         % nBits: Número de bits a simular
+        % h: Filtro h[n] (si es [] no hay filtro) 
     % Devuelve:
         % simBer: Vector de BER simulada
         
@@ -22,7 +23,17 @@ function simBER = simulateDNPSK_BER(SNR_dB,N,NFFT,Nofdm,N_pay,nBits)
         SNR = SNR_dB(i);
         % Se añade ruido para conseguir el SNR
         fb = 10*log10((NFFT/2)/N_pay);
-        y = awgn(x,SNR-fb,'measured');
+        if h
+            % Convolución de la señal con el filtro
+            y = conv(x,h);
+            % Incluye el del ruido
+            y = awgn(y,SNR-fb,'measured');
+            % Recortar la señal de salida
+            y = y(1:end-(length(h)-1));
+        else
+           % Inclusion del ruido
+            y = awgn(x,SNR-fb,'measured'); 
+        end
         % Demodulación
         rxBits = demodDNPSK(y,N,NFFT,Nofdm);
         % Aleatorización
@@ -31,4 +42,3 @@ function simBER = simulateDNPSK_BER(SNR_dB,N,NFFT,Nofdm,N_pay,nBits)
         simBER(i) = sum(abs(unrandomizedBits-txBits))/length(txBits);
     end
 end
-
